5ed98bdd007a036df69640f916eb8b173afc530e
Ayush Modi <hot9cups@yahoo.in>
2020-09-30 21:39:37 +0530

    Modified Json Parser, cleaner output

--- jsonParser/jokesToJson.py
+++ jsonParser/jokesToJson.py
@@ -10,8 +10,8 @@ with open("readme.md", "r", encoding="utf8") as file:
 
     for idx, joke in enumerate(separatedjokes[1:], 1):
         # skipping the first joke because that's just repo header
-        jokes["joke#" + str(idx)] = repr(joke.strip("\n").strip("#"))
-        # '\n' split to remove redundant newlines at beginning & end of joke
+        jokes["joke#" + str(idx)] = joke.strip().strip("#")
+        # remove redundant \n, \t and whitespaces at beginning & end of joke
         # '#' split because certain jokes have a ####Ques ####Answer format
 
 with open("Jokes.json", "w") as outfile:
