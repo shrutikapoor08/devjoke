437ecb817bbaeb95e48dadd54bef0bcd8aa5cd22
Ayush Modi <hot9cups@yahoo.in>
2020-09-30 21:35:11 +0530

    Added Joke to Json Parser

---
+++ jsonParser/jokesToJson.py
@@ -0,0 +1,18 @@
+import json
+
+jokes = {}
+with open("readme.md", "r", encoding="utf8") as file:
+    text = file.read()
+    text = text.replace(
+        "***", "* * *"
+    )  # Because some jokes aren't delimited by spaced-out asterisks
+    separatedjokes = text.split("* * *")
+
+    for idx, joke in enumerate(separatedjokes[1:], 1):
+        # skipping the first joke because that's just repo header
+        jokes["joke#" + str(idx)] = repr(joke.strip("\n").strip("#"))
+        # '\n' split to remove redundant newlines at beginning & end of joke
+        # '#' split because certain jokes have a ####Ques ####Answer format
+
+with open("Jokes.json", "w") as outfile:
+    json.dump(jokes, outfile)
